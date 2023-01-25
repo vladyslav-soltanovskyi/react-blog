aa00a7413000af7d839b949599d69963f4169e3b
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2023-01-25 14:59:49 +0100

    fix structure

--- client/src/services/api/axios/index.js
+++ client/src/services/api/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const $api = axios.create({
-  baseURL: "https://react-blog-api-azj3.onrender.com"
+  baseURL: "https://react-blog-api-azj3.onrender.com/"
 });
 
 $api.interceptors.request.use((config) => {
